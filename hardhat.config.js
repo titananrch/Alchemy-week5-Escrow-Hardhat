af9c78ac0cf6a77676199a0c3c7de44a8661c9a9
Titan Aji Nurceha <titanajinurceha@gmail.com>
2025-12-07 12:11:58 +0700

    Update network with sepolia testnet

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,7 @@
 require('@nomicfoundation/hardhat-toolbox');
+require("dotenv").config({ path: '.env' });
+
+const { PRIVATE_KEY, ALCHEMY_API_KEY } = process.env;
 
 module.exports = {
   solidity: "0.8.17",
@@ -6,8 +9,9 @@ module.exports = {
     artifacts: "./app/src/artifacts",
   },
   networks: {
-    hardhat: {
-      chainId: 1337,
+    sepolia: {
+      url: ALCHEMY_API_KEY,
+      accounts: PRIVATE_KEY ? [PRIVATE_KEY] : [],
     },
   },
 };
